6eebfe1a8312f8bb3cf9a91ad15f31ebbc4aeefa
spriteboysz <sprite_bx@126.com>
2023-08-22 23:36:13 +0800

    2023082223

---
+++ algorithm/P1757.sql
@@ -0,0 +1,27 @@
+create database P1757;
+use P1757;
+
+Create table If Not Exists Products
+(
+    product_id int,
+    low_fats   ENUM ('Y', 'N'),
+    recyclable ENUM ('Y','N')
+);
+Truncate table Products;
+insert into Products (product_id, low_fats, recyclable)
+values ('0', 'Y', 'N');
+insert into Products (product_id, low_fats, recyclable)
+values ('1', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable)
+values ('2', 'N', 'Y');
+insert into Products (product_id, low_fats, recyclable)
+values ('3', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable)
+values ('4', 'N', 'N');
+select *
+from Products;
+select product_id
+from Products
+where low_fats = 'Y'
+  and recyclable = 'Y';
+show databases;
