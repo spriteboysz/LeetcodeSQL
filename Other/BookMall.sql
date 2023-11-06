163093950384d6adf97156f94543a71dc09274c8
spriteboysz <sprite_bx@126.com>
2023-11-06 23:05:09 +0800

    2023110623

--- Other/BookMall.sql
+++ Other/BookMall.sql
@@ -11,11 +11,56 @@ create table users (
 );
 
 create table books (
-    book_num    char(8) primary key,
+    book_id     char(6) primary key,
     book_name   varchar(20)  not null,
     book_author varchar(20)  not null,
     book_cover  varchar(100) not null,
-    book_price  float        not null,
-    book_desc   varchar(100) not null,
-    book_stock  int          not null
+    book_price  double       not null,
+    book_desc   varchar(100),
+    book_stock  int          not null,
+    book_type   int          not null
 );
+show tables;
+
+
+select user_id       userId,
+       user_name     userName,
+       user_password userPassword,
+       user_realname userRealName,
+       user_gender   userGender,
+       user_tel      userTel,
+       user_desc     userDesc
+from users
+where user_name = 'admin';
+
+use bookmall;
+
+select book_id     bookId,
+       book_name   bookName,
+       book_author bookAuthor,
+       book_cover  bookCover,
+       book_price  bookPrice,
+       book_desc   bookDesc,
+       book_stock  bookStock,
+       book_type   bookType
+from books;
+
+select book_id     bookId,
+       book_name   bookName,
+       book_author bookAuthor,
+       book_cover  bookCover,
+       book_price  bookPrice,
+       book_desc   bookDesc,
+       book_stock  bookStock,
+       book_type   bookType
+from books
+where book_id = '202301';
+update books
+set book_name=?,
+    book_author=?,
+    book_cover=?,
+    book_price=?,
+    book_desc=?,
+    book_stock=?,
+    book_type=?
+where book_id=?
---
+++ Other/School.sql
@@ -0,0 +1,2 @@
+create database school;
+use school;
